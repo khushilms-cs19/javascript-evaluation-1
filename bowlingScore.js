40aceb6f287222e55d2ae3cece82c716c6fac718
khushilms <khushilms.cs19@bmsce.ac.in>
2023-01-18 13:31:30 +0530

    chore: remove log statements and clean code

--- bowlingScore.js
+++ bowlingScore.js
@@ -18,11 +18,9 @@ const isTenFrameGame = (rolls) => {
     }
   }
   const leftOverRolls = rolls.length - i - 1;
-  console.log(leftOverRolls);
   if (leftOverRolls === 2 || leftOverRolls === 3) {
     noOfFrames += 1;
   }
-  console.log(noOfFrames);
   if (noOfFrames === 10) {
     return true;
   }
@@ -38,12 +36,12 @@ const getRunningScore = (rolls) => {
   let newFrame = true;
   let i = 0;
   for (i = 0; i < rolls.length; i++) {
-    // console.log(eachFrameScore, frameCounter, i);
+
     if (frameCounter === 9) {
       break;
     }
     if (rolls[i] === 10) {
-      // finalScore += 10 + rolls[i + 1] + rolls[i + 2];
+
       eachFrameScore[frameCounter] = 10 + rolls[i + 1] + rolls[i + 2];
       frameCounter += 1;
       continue;
@@ -60,19 +58,19 @@ const getRunningScore = (rolls) => {
       newFrame = true;
     }
   }
-  // if (rolls[i] === 10) {
-  //   eachFrameScore[frameCounter] = rolls[i] + rolls[i + 1] + rolls[i + 2];
-  // }
-  // console.log("finish 9 frames", i, frameCounter);
+
+
+
+
   for (let j = i; j < rolls.length; j++) {
-    // console.log("here", eachFrameScore);
+
     eachFrameScore[frameCounter] += rolls[j];
   }
-  // console.log(eachFrameScore);
+
   return eachFrameScore.reduce((totalScore, currFrameScore) => totalScore + currFrameScore, 0);
 };
 
-// console.log(getRunningScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]));
+
 
 const getBestScoreOfAll = (games) => {
   return games.reduce((maxScore, game) => {
